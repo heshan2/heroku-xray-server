a3a773506a4ad2c2d05c7e21bb212bb55048bf5e
heshan2 <87757215+heshan2@users.noreply.github.com>
2021-08-02 21:21:28 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+#Config xray
+
+rm -rf /etc/xray/config.json
+cat << EOF > /etc/xray/config.json
+{
+  "inbounds": [
+    {
+      "port": $PORT,
+      "protocol": "vless",
+      "settings": {
+        "decryption": "none",
+        "clients": [
+          {
+            "id": "$UUID"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}
+EOF
+
+#run xray
+
+xray -c /etc/xray/config.json
